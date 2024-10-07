5614d543591564ede695ec55cb86cab4ad7bc418
Raizsa Zahra <117949449+zhrnyy@users.noreply.github.com>
2024-10-08 01:59:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './style.css'
 
 import * as THREE from "three";
-import {OrbitControls} from "node_modules/three/examples/jsm/controls/OrbitControls.js";
-import {ARButton} from "node_modules/three/addons/webxr/ARButton.js";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
+import {ARButton} from "three/addons/webxr/ARButton.js";
 import CasualFlapMapImageUrl from "./CasualFlatMap.png";
 
 let camera, scene, renderer, controls;
